f18d45f6c9edc6c4b83fd06090e91cf12f7150c1
melyguy <nidojohan@gmail.com>
2025-11-10 09:14:03 +0100

    Add project files.

---
+++ FileOrganizer/Organizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public static class FileOrganizer
+{
+    private static readonly Dictionary<string, string> fileCategories = new()
+    {
+        { ".jpg", "Images" }, { ".png", "Images" }, { ".gif", "Images" },
+        { ".mp4", "Videos" }, { ".mov", "Videos" },
+        { ".pdf", "Documents" }, { ".docx", "Documents" }, { ".txt", "Documents" },
+        { ".zip", "Archives" }, { ".rar", "Archives" },
+        { ".exe", "Software" }, { ".msi", "Software" }
+    };
+
+}
---
+++ FileOrganizer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the folder path to organize:");
+        string? folderPath = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(folderPath) || !Directory.Exists(folderPath))
+        {
+            Console.WriteLine("Invalid folder path");
+            return;
+        }
+
+        FileOrganizer.Organize(folderPath);
+
+        Console.WriteLine("Organization complete!");
+    }
+}
